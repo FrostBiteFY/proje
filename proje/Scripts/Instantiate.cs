555921c7f6e04002d65fe8cf497d902ad6e23e80
FrostBiteFY <51825679+FrostBiteFY@users.noreply.github.com>
2020-06-24 17:12:54 +0300

    Add files via upload

---
+++ proje/Scripts/Instantiate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    public GameObject prefab;
+    // public Camera mainCamera;
+    public Camera cam;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 touchPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));//mousenın positionunu aldı vector 3 e atadı.
+
+            Instantiate(prefab, touchPos, Quaternion.identity);
+        }
+    }
+}
